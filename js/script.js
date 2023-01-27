85ce9a710a852b87304cde16e94a3646f730550b
fpresta0607 <82167077+fpresta0607@users.noreply.github.com>
2023-01-27 11:52:31 -0600

    edited style formating

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 const homeLink = document.querySelector('nav li:first-child a');
-const projectsLink = document.querySelector('nav li:last-child a');
-
+const projectsLink = document.querySelector('nav li:nth-child(2) a');
+const aboutLink = document.querySelector('nav li:last-child a');
 
 homeLink.addEventListener('click', (event) => {
     event.preventDefault(); 
     window.location.href = '/';
 });
 
-
 projectsLink.addEventListener('click', (event) => {
     event.preventDefault(); 
     window.location.href = '/projects';
 });
+
+aboutLink.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    window.location.href = '/about';
+});
+
