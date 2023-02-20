c24044c6dd0ac8ad7ed608c00bcdf555e8f97e95
fpresta0607 <82167077+fpresta0607@users.noreply.github.com>
2023-02-20 17:40:34 -0600

    added web app music generator

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const homeLink = document.querySelector('nav li:first-child a');
 const projectsLink = document.querySelector('nav li:nth-child(2) a');
-const aboutLink = document.querySelector('nav li:last-child a');
+const aboutLink = document.querySelector('nav li:nth-child(3) a');
+const BBLink = document.querySelector('nav li:last-child a');
 
 homeLink.addEventListener('click', (event) => {
     event.preventDefault(); 
@@ -15,5 +16,10 @@ projectsLink.addEventListener('click', (event) => {
 aboutLink.addEventListener('click', (event) => {
     event.preventDefault(); 
     window.location.href = '/about';
+});
+
+BBLink.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    window.location.href = '/beatBoost';
 });
 
